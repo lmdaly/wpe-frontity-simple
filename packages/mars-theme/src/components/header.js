48446be1c0167166eeadf9c80fcd079b2e0b4c18
Konrad Głowacki <konrad.glowacki@wpengine.com>
2020-11-12 11:25:54 +0100

    Add local build

--- packages/mars-theme/src/components/header.js
+++ packages/mars-theme/src/components/header.js
@@ -10,6 +10,7 @@ const Header = ({ state }) => {
       <Container>
         <StyledLink link="/">
           <Title>{state.frontity.title}</Title>
+          <p>How are ya all?</p>
         </StyledLink>
         <Description>{state.frontity.description}</Description>
         <MobileMenu />
